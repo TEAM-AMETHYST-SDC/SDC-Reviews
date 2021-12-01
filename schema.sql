c2d4108d6c6886b33d3537a012efbbf478058bba
Guillermo Martinez <gmartinez948@outlook.com>
2021-12-01 18:47:23 -0500

    reverted directory changes

--- schema.sql
+++ schema.sql
@@ -54,8 +54,8 @@ CREATE INDEX p_id_idx ON photos(review_id);
 
 
 
-COPY reviews FROM '/Users/guillermo/Public/Drop Box/data/reviews.csv'  DELIMITER ',' CSV HEADER;
-COPY characteristics FROM '/Users/guillermo/Public/Drop Box/data/characteristics.csv' DELIMITER ',' CSV HEADER;
-COPY characteristic_reviews FROM '/Users/guillermo/Public/Drop Box/data/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
-COPY photos FROM '/Users/guillermo/Public/Drop Box/data/reviews_photos.csv' DELIMITER ',' CSV HEADER;
+COPY reviews FROM '/Users/guillermo/Desktop/data/reviews.csv'  DELIMITER ',' CSV HEADER;
+COPY characteristics FROM '/Users/guillermo/Desktop/data/characteristics.csv' DELIMITER ',' CSV HEADER;
+COPY characteristic_reviews FROM '/Users/guillermo/Desktop/data/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/guillermo/Desktop/data/reviews_photos.csv' DELIMITER ',' CSV HEADER;
 
